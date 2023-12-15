148d92790d6c6dfb358079c89a0e7039b600907f
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-15 17:05:50 -0300

    create title component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Link from 'next/link'
+import { Title } from '@/components/typografy'
 
 export const metadata: Metadata = {
   title: 'Gvão movies',
@@ -17,7 +18,9 @@ export default function RootLayout({
       <body>
 
         <header>
-          <Link href={'/'}>Gvão Movies</Link>
+          <Link href={'/'}>
+            <Title>Gvão Movies</Title>
+          </Link>
         </header>
 
         {children}
---
+++ src/components/typografy.tsx
@@ -0,0 +1,7 @@
+export const Title = ({ children }: { children: React.ReactNode }) => (
+    <h1
+        className="text-2xl font-bold text-center"
+    >
+        {children}
+    </h1>
+)
