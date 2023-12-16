96f9c78f361956bfceb6776faace62bd88cc41db
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-15 22:48:38 -0300

    insert filter by type

--- src/app/trends/page.tsx
+++ src/app/trends/page.tsx
@@ -2,29 +2,34 @@ import { SubTitle, Title } from "@/components/typography";
 import { MovieDetails, TMDB } from "@/domain/entities/tmdb";
 import Link from "next/link";
 
-export default async function TrendPage({ searchParams }: { searchParams: { page: string } }) {
-  const { page } = searchParams
+export default async function TrendPage({ searchParams }: { searchParams: { page: string, type: string } }) {
+  const { page, type } = searchParams
 
   return (
     <section>
 
       <div className="container">
-        
+
         <div className="bg-cyan-500 h-28 flex justify-center items-center">
           <Title>GM</Title>
         </div>
 
-        <TrendsList page={+page || 1} />
+        <TrendsList page={+page || 1} filterBy={type} />
 
       </div>
     </section>
   )
 }
 
-const TrendsList = async ({ page }: { page: number }) => {
+const TrendsList = async ({ page, filterBy }: { page: number, filterBy: string }) => {
   const tmdb = new TMDB()
-
   const { results } = await tmdb.getTrends({ page })
+
+  const typesRecords = results
+    .reduce((acc: Record<string, number>, record) => {
+      acc[record.media_type] = acc[record.media_type] + 1 || 1
+      return acc
+    }, {})
 
   const pagesToNavigation = new Array(10).fill(null).map((_, i) => page + i)
 
@@ -34,23 +39,50 @@ const TrendsList = async ({ page }: { page: number }) => {
   }
 
   return <>
+    <ul className="space-x-4 items-center my-auto p-4">
+      {
+        Object.entries(typesRecords)
+          .map(([type, value]) => (
+            <Link
+              key={type}
+              className={`p-2 rounded shadow opacity-80 ${type === filterBy && `bg-cyan-500 opacity-100`}`}
+              href={{
+                query: { page, type }
+              }}
+            >
+              {type}:{value}
+            </Link>
+          ))
+      }
+    </ul>
+
     <ul className="flex flex-wrap gap-4 p-4" >
-      {results.map(result => (
-        <Link href={`/#`} className="cursor-pointer" key={result.id}>
-          <TrendItem movie={result} />
-        </Link>
-      ))}
+      {
+        results
+          .filter(result => {
+            if (!filterBy) return true
+            return result.media_type === filterBy
+          })
+          .map(result => (
+            <Link href={`/trends/${result.media_type}/${result.id}`} className="cursor-pointer" key={result.id}>
+              <TrendItem movie={result} />
+            </Link>
+          ))
+      }
     </ul>
 
     <section id="pagination" className="flex justify-evenly items-center">
-      {pagesToNavigation.map(pageNumber => (
-        <Link
-          key={`page-${pageNumber}`}
-          className={getStyleIfEqualPage(pageNumber)}
-          href={`?page=${pageNumber}`}>
-          {pageNumber}
-        </Link>
-      ))}
+      {
+        pagesToNavigation
+          .map(pageNumber => (
+            <Link
+              key={`page-${pageNumber}`}
+              className={getStyleIfEqualPage(pageNumber)}
+              href={`?page=${pageNumber}`}>
+              {pageNumber}
+            </Link>
+          ))
+      }
     </section>
   </>
 }
@@ -64,6 +96,8 @@ const TrendItem = ({ movie, ...props }: { movie: MovieDetails }) => (
       {movie.name || movie.title}
     </h3>
     <img src={TMDB.getImage(movie.backdrop_path)} alt="" className="w-full" />
+    <p>type: {movie.media_type}</p>
     <p className="p-1 translate-x-50">{movie.overview}</p>
+
   </li>
 )
