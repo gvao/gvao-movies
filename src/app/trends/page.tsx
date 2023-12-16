5f62916deb92fcf01ff8b0ba988f86852aeb76c9
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-15 21:50:36 -0300

    create Trends pagination

--- src/app/trends/page.tsx
+++ src/app/trends/page.tsx
@@ -2,41 +2,55 @@ import { SubTitle, Title } from "@/components/typography";
 import { MovieDetails, TMDB } from "@/domain/entities/tmdb";
 import Link from "next/link";
 
-export default async function Home({ params: { page } }: { params: { page: string } }) {
+export default async function TrendPage({ searchParams }: { searchParams: { page: string } }) {
+  const { page } = searchParams
 
   return (
     <section>
 
       <div className="container">
+        
         <div className="bg-cyan-500 h-28 flex justify-center items-center">
           <Title>GM</Title>
         </div>
 
-        {/* <SubTitle>Melhores filmes</SubTitle> */}
-
-        <Trends page={+page || 1} />
+        <TrendsList page={+page || 1} />
 
       </div>
     </section>
   )
 }
 
-const Trends = async ({ page }: { page: number }) => {
+const TrendsList = async ({ page }: { page: number }) => {
   const tmdb = new TMDB()
 
   const { results } = await tmdb.getTrends({ page })
 
+  const pagesToNavigation = new Array(10).fill(null).map((_, i) => page + i)
+
+  const getStyleIfEqualPage = (pageNumber: number) => {
+    if (pageNumber === page) return "bg-cyan-500 aspect-square p-2 rounded font-semibold"
+    return ""
+  }
+
   return <>
     <ul className="flex flex-wrap gap-4 p-4" >
       {results.map(result => (
-        <Link href={`#`} className="cursor-pointer" >
+        <Link href={`/#`} className="cursor-pointer" key={result.id}>
           <TrendItem movie={result} />
         </Link>
       ))}
     </ul>
 
-    <section id="pagination">
-
+    <section id="pagination" className="flex justify-evenly items-center">
+      {pagesToNavigation.map(pageNumber => (
+        <Link
+          key={`page-${pageNumber}`}
+          className={getStyleIfEqualPage(pageNumber)}
+          href={`?page=${pageNumber}`}>
+          {pageNumber}
+        </Link>
+      ))}
     </section>
   </>
 }
