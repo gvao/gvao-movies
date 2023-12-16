0939bc4ac23e68a38ccddbc3f86f0426d537e1a3
Yuri Galvao <yuri.gvao@gmail.com>
2023-12-16 13:04:11 -0300

    create get tv by id method

--- src/domain/entities/tmdb.ts
+++ src/domain/entities/tmdb.ts
@@ -62,6 +62,96 @@ type Movie = {
     vote_count: number
 }
 
+type Tv = {
+    adult: boolean,
+    backdrop_path: string,
+    created_by: {
+        id: number,
+        credit_id: string,
+        name: string,
+        gender: number,
+        profile_path: string | null
+    }[],
+    episode_run_time: [],
+    first_air_date: string,
+    genres: { id: number, name: string }[],
+    homepage: string,
+    id: number,
+    in_production: boolean,
+    languages: string[],
+    last_air_date: string,
+    last_episode_to_air: {
+        id: number,
+        name: string,
+        overview: string,
+        vote_average: number,
+        vote_count: number,
+        air_date: string,
+        episode_number: number,
+        episode_type: string,
+        production_code: string,
+        runtime: number,
+        season_number: number,
+        show_id: number,
+        still_path: string
+    },
+    name: string,
+    next_episode_to_air: string | null,
+    networks: {
+        id: number,
+        logo_path: string,
+        name: string,
+        origin_country: string
+    }[],
+    number_of_episodes: number,
+    number_of_seasons: number,
+    origin_country: string[],
+    original_language: string,
+    original_name: string,
+    overview: string,
+    popularity: number,
+    poster_path: string,
+    production_companies: {
+        id: number,
+        logo_path: string,
+        name: string,
+        origin_country: string
+    }[],
+    production_countries: { iso_3166_1: string, name: string }[],
+    seasons: {
+        air_date: string,
+        episode_count: number,
+        id: number,
+        name: string,
+        overview: string,
+        poster_path: string,
+        season_number: number,
+        vote_average: number
+    }[],
+    spoken_languages: { english_name: string, iso_639_1: string, name: string }[],
+    status: string,
+    tagline: string,
+    type: string,
+    vote_average: number,
+    vote_count: number
+}
+
+const getOptions = (apiKey: string, method: "get" | "post" = "get"): RequestInit => {
+    const object: Record<string, RequestInit> = {
+        get: {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${apiKey}`
+            },
+            next: {
+                revalidate: 60,
+            }
+        }
+    }
+
+    return object[method]
+}
 
 export class TMDB {
     private accountId = process.env.TMDB_ACCOUNT_ID
@@ -72,35 +162,29 @@ export class TMDB {
         return `https://image.tmdb.org/t/p/w500/${path}`
     }
 
+    async getTvById(series_id: string): Promise<Tv> {
+        const path = `/3/tv/${series_id}?language=pt-BR`;
+        const url = this.URL_BASE + path
+        const options = getOptions(this.apiKey!)
+
+        const response = await fetch(url, options)
+
+        return await response.json()
+    }
     async getMovieById(id: string): Promise<Movie> {
         const path = `/3/movie/${id}?language=pt-BR`;
         const url = this.URL_BASE + path
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${this.apiKey}`
-            }
-        };
+        const options = getOptions(this.apiKey!)
 
         const response = await fetch(url, options)
 
         return await response.json()
     }
 
-    getTrends = async ({ page = 1, time_window = 'day' }: InputTrends): Promise<OutputTrends> => {
+    async getTrends({ page = 1, time_window = 'day' }: InputTrends): Promise<OutputTrends> {
 
         const url = `${this.URL_BASE}/3/trending/all/${time_window}?language=pt-BR&page=${page}`;
-        const options: RequestInit = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${this.apiKey}`
-            },
-            next: {
-                revalidate: 60,
-            }
-        };
+        const options = getOptions(this.apiKey!)
 
         return fetch(url, options)
             .then(res => res.json())
@@ -112,18 +196,11 @@ export class TMDB {
         return await response.json()
     }
 
-
     async getAccount() {
         const url = this.URL_BASE + `/3/account/${this.accountId}`;
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjJmYTY0ZjAxY2QxMTE0NDBkMmQyOTA2NDk3NWFiMyIsInN1YiI6IjY1N2M5MDA4NTY0ZWM3MDBhY2Q2OGQ4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GLPOUvdYYRNQVwUj9aD_zscxx2Kcc_z8uuVwJPrvvHw'
-            }
-        };
+        const options = getOptions(this.apiKey!)
 
         const response = await fetch(url, options)
-        return await response.json()
+        return response.json()
     }
 }
